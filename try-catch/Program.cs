28ea0f102a88e705ec39f1c795d20aeab0cf08bb
Dominik Techl <techldo20@sps-prosek.cz>
2023-02-27 13:24:46 +0100

    Přidejte soubory projektu.

---
+++ try-catch/Program.cs
@@ -0,0 +1,58 @@
+﻿
+using System;
+
+namespace trycatch
+{
+    class Program
+    {
+        static void Main()
+        {
+            int konec = 0;
+
+            while (konec == 0)
+            {
+                int[] numArray = { 1, 2, 3, 4, 5, 6 };
+
+                Console.WriteLine("Napište číslo");
+                Console.Write("->");
+                int cislo;
+
+
+                while (!Int32.TryParse(Console.ReadLine(), out cislo))
+                {
+                    Console.WriteLine("Napište číslo");
+                    Console.Write("->");
+                }
+
+                Console.WriteLine();
+
+                try
+                {
+                    if (cislo < 0)
+                    {
+                        throw new NegativeNumberException("Číslo nesmí být záporné");
+                    }
+
+                    Console.WriteLine(numArray[cislo]);
+                }
+                catch (IndexOutOfRangeException x)
+                {
+                    Console.WriteLine("Vyskytla se chyba: " + x.Message);
+                }
+                catch (NegativeNumberException x) 
+                {
+                    Console.WriteLine(x.Message);
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+
+    class NegativeNumberException : Exception
+    {
+        public NegativeNumberException() { }
+
+        public NegativeNumberException(string message) : base(message) {}
+    }
+}
